9cc1bdf74d9ccb302ab0c17d0177d526d3b3c428
Gab Fiterman <gfiterman96@gmail.com>
2022-09-13 12:45:47 -0300

    default fetch

---
+++ layouts/default.vue
@@ -0,0 +1,22 @@
+<template>
+    <div id="body">
+        <Nuxt />
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+#body {
+    background-color: rgb(34, 34, 34);
+    color: rgb(240, 248, 255);
+    padding: 2rem;
+    margin: 0;
+    min-height: 100vh;
+    font-family: 'poppins', 'arial', sans-serif;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <p v-if="$fetchState.pending">Fetching mountains...</p>
+    <p v-else-if="$fetchState.error">An error occurred :(</p>
+    <div v-else>
+      <h1>Nuxt Mountains</h1>
+      <ul>
+        <li v-for="mountain of mountains" :key="mountain">
+          <h1>{{ mountain.title }}</h1>
+          <img :src="mountain.image">
+        </li>
+      </ul>
+      <button @click="$fetch">Refresh</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      mountains: []
+    }
+  },
+  async fetch() {
+    this.mountains = await fetch(
+      'https://api.nuxtjs.dev/mountains'
+    ).then(res => res.json())
+  }
+}
+</script>
